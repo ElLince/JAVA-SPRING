4a42f110e4ea6e2647815e24c28235f05b23b06e
Javier León <104456566+ElLince@users.noreply.github.com>
2023-04-20 20:02:32 +0200

    Add files via upload

---
+++ spring2/src/main/java/com/spring/spring/EmpleadoJefe.java
@@ -0,0 +1,17 @@
+package com.spring.spring;
+
+public class EmpleadoJefe implements Empleados{
+    EmpleadoJefe(){
+        super();
+    }
+
+    public String getResponsabilidades() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getResponsabilidades'");
+    }
+
+    public String getDocumento() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDocumento'");
+    }
+}
