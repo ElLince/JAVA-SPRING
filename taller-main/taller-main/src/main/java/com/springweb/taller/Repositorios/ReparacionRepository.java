44309225be0077eb875a30c8f5792d89392c8d18
Javier León <104456566+ElLince@users.noreply.github.com>
2023-05-02 19:27:01 +0200

    Delete taller-main directory

--- taller-main/taller-main/src/main/java/com/springweb/taller/Repositorios/ReparacionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springweb.taller.Repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.springweb.taller.Modelo.Reparacion;
-
-
-public interface ReparacionRepository extends JpaRepository<Reparacion, Long> {
-}
